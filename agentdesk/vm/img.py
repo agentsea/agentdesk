68d7ba042d34ad6788f2a385c22e232f59caf6b9
Patrick Barker <patrickbarkerco@gmail.com>
2024-03-21 15:58:38 -0600

    update default images

--- agentdesk/vm/img.py
+++ agentdesk/vm/img.py
@@ -16,7 +16,7 @@ class Image:
 JAMMY = Image(
     "jammy",
     "Ubuntu 22.04 Jammy server with agentd",
-    gce="agentd-ubuntu-22-04-20240320112030",
-    ec2="ami-043878e4f4215adca",
+    gce="agentd-ubuntu-22-04-20240321084622",
+    ec2="ami-034597756c87b5f6e",
     qcow2="https://storage.googleapis.com/agentsea-vms/jammy/latest/agentd-jammy.qcow2",
 )
