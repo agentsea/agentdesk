3f8c12c3240376c2e09d217e1b1a2c3586a274c0
Patrick Barker <patrickbarkerco@gmail.com>
2024-05-29 16:07:43 -0600

    update opts

--- agentdesk/device.py
+++ agentdesk/device.py
@@ -481,38 +481,27 @@ class Desktop(Device):
         """Press a key
 
         Args:
-            key (str: Which key to press.
-                Options are:
-                    [
-                        "\t", "\n", "\r", " ", "!", '"', "#", "$", "%", "&", "'",
-                        "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3",
-                        "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">",
-                        "?", "@", "[", "\\", "]", "^", "_", "`", "a", "b", "c",
-                        "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-                        "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y",
-                        "z", "{", "|", "}", "~", "accept", "add", "alt", "altleft",
-                        "altright", "apps", "backspace", "browserback",
-                        "browserfavorites", "browserforward", "browserhome",
-                        "browserrefresh", "browsersearch", "browserstop",
-                        "capslock", "clear", "convert", "ctrl", "ctrlleft",
-                        "ctrlright", "decimal", "del", "delete", "divide", "down",
-                        "end", "enter", "esc", "escape", "execute", "f1", "f10",
-                        "f11", "f12", "f13", "f14", "f15", "f16", "f17", "f18",
-                        "f19", "f2", "f20", "f21", "f22", "f23", "f24", "f3",
-                        "f4", "f5", "f6", "f7", "f8", "f9", "final", "fn", "hanguel",
-                        "hangul", "hanja", "help", "home", "insert", "junja", "kana",
-                        "kanji", "launchapp1", "launchapp2", "launchmail",
-                        "launchmediaselect", "left", "modechange", "multiply",
-                        "nexttrack", "nonconvert", "num0", "num1", "num2", "num3",
-                        "num4", "num5", "num6", "num7", "num8", "num9", "numlock",
-                        "pagedown", "pageup", "pause", "pgdn", "pgup", "playpause",
-                        "prevtrack", "print", "printscreen", "prntscrn", "prtsc",
-                        "prtscr", "return", "right", "scrolllock", "select",
-                        "separator", "shift", "shiftleft", "shiftright", "sleep",
-                        "space", "stop", "subtract", "tab", "up", "volumedown",
-                        "volumemute", "volumeup", "win", "winleft", "winright",
-                        "yen", "command", "option", "optionleft", "optionright"
-                    ]
+            key (str): Which key to press. Options are:
+                [ "\\t", "\\n", "\\r", " ", "!", '\\"', "\\#", "\\$", "\\%", "\\&", "\\'",
+                "\\(", "\\)", "\\*", "\\+", ",", "-", "\\.", "/", "0", "1", "2", "3",
+                "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "\\?", "@",
+                "\\[", "\\\\", "\\]", "\\^", "\\_", "\\`", "a", "b", "c", "d", "e",
+                "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+                "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~", "accept", "add",
+                "alt", "altleft", "altright", "apps", "backspace", "browserback",
+                "browserfavorites", "browserforward", "browserhome", "browserrefresh",
+                "browsersearch", "browserstop", "capslock", "clear", "convert", "ctrl",
+                "ctrlleft", "ctrlright", "decimal", "del", "delete", "divide", "down",
+                "end", "enter", "esc", "escape", "execute", "f1", "f10", "f11", "f12",
+                "f13", "f14", "f15", "f16", "f17", "f18", "f19", "f2", "f20", "f21",
+                "f22", "f23", "f24", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "final",
+                "fn", "help", "home", "insert", "left", "numlock", "pagedown", "pageup", "pause",
+                "pgdn", "pgup", "playpause", "prevtrack", "print", "printscreen",
+                "prntscrn", "prtsc", "prtscr", "return", "right", "scrolllock",
+                "select", "separator", "shift", "shiftleft", "shiftright", "sleep",
+                "space", "stop", "subtract", "tab", "up", "volumedown", "volumemute",
+                "volumeup", "win", "winleft", "winright", "yen", "command", "option",
+                "optionleft", "optionright" ]
         """
         requests.post(f"{self.base_url}/press_key", json={"key": key})
         return
@@ -522,38 +511,27 @@ class Desktop(Device):
         """Press a hot key. For example ctrl+c
 
         Args:
-            keys (List[str]): Which keys to press.
-                Options are:
-                    [
-                        "\t", "\n", "\r", " ", "!", '"', "#", "$", "%", "&", "'",
-                        "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3",
-                        "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">",
-                        "?", "@", "[", "\\", "]", "^", "_", "`", "a", "b", "c",
-                        "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-                        "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y",
-                        "z", "{", "|", "}", "~", "accept", "add", "alt", "altleft",
-                        "altright", "apps", "backspace", "browserback",
-                        "browserfavorites", "browserforward", "browserhome",
-                        "browserrefresh", "browsersearch", "browserstop",
-                        "capslock", "clear", "convert", "ctrl", "ctrlleft",
-                        "ctrlright", "decimal", "del", "delete", "divide", "down",
-                        "end", "enter", "esc", "escape", "execute", "f1", "f10",
-                        "f11", "f12", "f13", "f14", "f15", "f16", "f17", "f18",
-                        "f19", "f2", "f20", "f21", "f22", "f23", "f24", "f3",
-                        "f4", "f5", "f6", "f7", "f8", "f9", "final", "fn", "hanguel",
-                        "hangul", "hanja", "help", "home", "insert", "junja", "kana",
-                        "kanji", "launchapp1", "launchapp2", "launchmail",
-                        "launchmediaselect", "left", "modechange", "multiply",
-                        "nexttrack", "nonconvert", "num0", "num1", "num2", "num3",
-                        "num4", "num5", "num6", "num7", "num8", "num9", "numlock",
-                        "pagedown", "pageup", "pause", "pgdn", "pgup", "playpause",
-                        "prevtrack", "print", "printscreen", "prntscrn", "prtsc",
-                        "prtscr", "return", "right", "scrolllock", "select",
-                        "separator", "shift", "shiftleft", "shiftright", "sleep",
-                        "space", "stop", "subtract", "tab", "up", "volumedown",
-                        "volumemute", "volumeup", "win", "winleft", "winright",
-                        "yen", "command", "option", "optionleft", "optionright"
-                    ]
+            keys (List[str]): Which keys to press. Options are:
+                [ "\\t", "\\n", "\\r", " ", "!", '\\"', "\\#", "\\$", "\\%", "\\&", "\\'",
+                "\\(", "\\)", "\\*", "\\+", ",", "-", "\\.", "/", "0", "1", "2", "3",
+                "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "\\?", "@",
+                "\\[", "\\\\", "\\]", "\\^", "\\_", "\\`", "a", "b", "c", "d", "e",
+                "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+                "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~", "accept", "add",
+                "alt", "altleft", "altright", "apps", "backspace", "browserback",
+                "browserfavorites", "browserforward", "browserhome", "browserrefresh",
+                "browsersearch", "browserstop", "capslock", "clear", "convert", "ctrl",
+                "ctrlleft", "ctrlright", "decimal", "del", "delete", "divide", "down",
+                "end", "enter", "esc", "escape", "execute", "f1", "f10", "f11", "f12",
+                "f13", "f14", "f15", "f16", "f17", "f18", "f19", "f2", "f20", "f21",
+                "f22", "f23", "f24", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "final",
+                "fn", "help", "home", "insert", "left", "numlock", "pagedown", "pageup", "pause",
+                "pgdn", "pgup", "playpause", "prevtrack", "print", "printscreen",
+                "prntscrn", "prtsc", "prtscr", "return", "right", "scrolllock",
+                "select", "separator", "shift", "shiftleft", "shiftright", "sleep",
+                "space", "stop", "subtract", "tab", "up", "volumedown", "volumemute",
+                "volumeup", "win", "winleft", "winright", "yen", "command", "option",
+                "optionleft", "optionright" ]
         """
         requests.post(f"{self.base_url}/hot_key", json={"keys": keys})
         return
--- agentdesk/vm/img.py
+++ agentdesk/vm/img.py
@@ -16,7 +16,7 @@ class Image:
 JAMMY = Image(
     "jammy",
     "Ubuntu 22.04 Jammy server with agentd",
-    gce="agentd-ubuntu-22-04-20240321084622",
-    ec2="ami-034597756c87b5f6e",
+    gce="agentd-ubuntu-22-04-20240529073401",
+    ec2="ami-06f3f5d8474ba4099",
     qcow2="https://storage.googleapis.com/agentsea-vms/jammy/latest/agentd-jammy.qcow2",
 )
